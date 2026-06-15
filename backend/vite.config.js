168b1d496117e1cb8291e80d168bd1297f02eb32
kangudindev <kangudindev@github.com>
2026-06-15 15:54:24 +0700

    fix: vite emptyOutDir=false to prevent 500 on build failure, fix deploy npm install

--- backend/vite.config.js
+++ backend/vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     build: {
         chunkSizeWarningLimit: 4000,
+        emptyOutDir: false,
     },
     server: {
         host: '0.0.0.0',
